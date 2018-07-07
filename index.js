0e3a888d535cb425426c0a996cd82f795b0f1a1c
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-07 19:56:35 +0900

    add

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = parcel => {
+  console.warn("This is dangerous plugin");
   // const { resolver } = parcel;
   parcel.addAssetType("js", require.resolve("./SassDartAsset.js"));
   parcel.addPackager("js", require.resolve("./SassDartPackager.js"));
