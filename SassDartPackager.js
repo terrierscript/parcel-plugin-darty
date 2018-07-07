a3bc23789f1bdc531f9646cd685d72d2ab7bb481
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-07 18:58:28 +0900

    add

--- SassDartPackager.js
+++ SassDartPackager.js
@@ -1,6 +1,9 @@
 const JSPackager = require("parcel-bundler/lib/packagers/JSPackager");
 
-const code = require("fs").readFileSync("./fs-mock.js");
+const code = require("fs").readFileSync(
+  path.join(__dirname, "./fs-mock.js"),
+  "utf-8"
+);
 
 module.exports = class SassDartPackager extends JSPackager {
   async end() {
