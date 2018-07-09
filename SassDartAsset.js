c861550fc77cdbd2d38458779ef752c70dce190b
terrierscript <13282103+terrierscript@users.noreply.github.com>
2018-07-09 09:54:15 +0900

    add asset

--- SassDartAsset.js
+++ SassDartAsset.js
@@ -7,8 +7,23 @@ module.exports = class SassDartAsset extends JSAsset {
   isTarget() {
     return this.name.indexOf("sass.dart.js") > 0;
   }
-
+  appendBuffer() {
+    this.addDependency("buffer");
+    this.globals.set(
+      "buffer",
+      'self.Buffer = require("buffer").Buffer;console.log("SDA")'
+    );
+  }
+  async pretransform() {
+    return;
+    // if (!this.isTarget()) {
+    //   return;
+    // }
+    // console.log("pretrans");
+    // this.appendBuffer();
+  }
   async postProcess(generated) {
+    console.log("postProcess");
     if (!this.isTarget()) {
       return generated;
     }
